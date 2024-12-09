5c91fdb9b2f4573283f5df54fb476280655e3e8b
Gaurav Bisht <gauravbisht7289@gmail.com>
2024-12-09 13:02:29 +0530

    Data_validation to feautre engineering configuration

--- training/exception.py
+++ training/exception.py
@@ -32,10 +32,9 @@ class DataIngestionError(PipelineError):
 
 class DataValidationError(PipelineError):
     pass
-class FeatureExtractionError(PipelineError):
-    pass
 
-class NestedCrossValError(PipelineError):
+
+class CrossValError(PipelineError):
     pass
 
 class FeatureEngineeringError(PipelineError):
